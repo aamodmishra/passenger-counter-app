b0527ec5ecf37ea4c0f3b6a502116b9711cb5e23
amod <aamodmishra111@example.com>
2024-12-20 19:11:20 +0545

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-document.getElementById("count-el").innerText=5
+document.getElementById("count-el").innerText="lets start counting"
+ 
+let count=0
+let countEL =document.getElementById("count-el")
+console.log(countEL); //this gives the html element that has class name called countEl
+
+function increment()
+{
+    count=count+1
+    
+    countEL.innerText=count
+}
